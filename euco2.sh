18b5c3755fc77a0edd3dc120211a01a11c089554
Jari Perttunen <jari.perttunen@luke.fi>
2021-04-20 10:16:21 +0300

    Completed sample command lines.

--- euco2.sh
+++ euco2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+#Examples to collect CRFReporter Reporting tables (Excel files)
+#Collect TableA-TableF for years 1990-20xx for each all countries
+python euco2.py euco2.py -d /data/d4/projects/khk/CRF_vertailu/ --all  -s 1990 -e 2019 
 #Using CRFReporter Reporting tables (Excel files)
-#Collect TableA-TableF for years 1990-2014 for each EU memeber state
-python euco2.py
-#Using CRFReporter Reporting tables (Excel files)
-#Collect Table4.Gs1 HWP for years 1990-2014 for each EU memeber state
-python euco2hwp.py
+#Collect Table4.Gs1 HWP for years 1990-20xx for each all countries
+python euco2hwp.py euco2.py -d /data/d4/projects/khk/CRF_vertailu/ --all  -s 1990 -e 2019 
