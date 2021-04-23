0d3252dbbda53e3d8ae564dcd85daa8430d27374
Jari Perttunen <jari.perttunen@luke.fi>
2021-04-23 11:44:33 +0300

    List of EU countries and list of all countries participating in GHG Inventory

---
+++ countrylist.py
@@ -0,0 +1,7 @@
+euls=['FIN','AUT','BEL','BGR','CYP','CZE','DEU',
+      'DNK','ESP','EST','FRA','GRC','HRV',
+      'HUN','IRL','ITA','LTU','LUX','LVA',
+      'MLT','NLD','POL','PRT','ROU','SVK',
+      'SVN','SWE']
+noneuls=['CAN','CHE','EUA','GBR','ISL','JPN','LIE','MCO','NOR','RUS','TUR','USA']
+allcountryls=euls+noneuls
