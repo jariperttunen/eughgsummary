203f527b89e65d113f426fbb1ca947445105820a
jariperttunen <jari.perttunen@icloud.com>
2023-11-30 12:43:53 +0200

    Producing HWP Table4.Gs1 Gains and Losses. Output is column-wise.

--- euhwpapproachbtotal.py
+++ euhwpapproachbtotal.py
@@ -9,9 +9,8 @@ from countrylist import euls,euplusls,noneuls,allcountryls
 hwp_table = 'Table4.Gs1'
 hwp_row = 'Total'
 hwp_row_total = 'TOTAL HWP'
-hwp_sheet_name_ls = ['Table4.Gs1_HWP_domestic_gains','Table4.Gs1_HWP_domestic_losses',
-                     'Table4.Gs1_HWP_exported_gains','Table4.Gs1_HWP_exported_losses']
-
+hwp_sheet_name = 'HWP Table4.Gs1'
+hwp_columns_ls = ["Country","Year","Gains(tC)","Gains(CO2)","TG/DG/EG","Losses(tC)","Losses(CO2)","TL/DL/EL"]
 def color_orange(val):
     """
     Highlight in orange, to be used with row selection of countries
@@ -19,6 +18,13 @@ def color_orange(val):
     """
     return  'background-color: orange'
 
+def convert_co2(x):
+    try:
+        x = 44.0/12.0*x
+        return x
+    except:
+        return x
+    
 def EUHwpApproachBTotal(excel_writer,directory:str,countryls:list,sheet:str,
                         from_row:str,from_row_total:str,col_gains:int,col_losses:int,start:int,end:int):
     """
@@ -36,68 +42,95 @@ def EUHwpApproachBTotal(excel_writer,directory:str,countryls:list,sheet:str,
     """
     #Keep track of the countries that have HWP Total only
     hwp_total_only_set=set()
-    domestic_gains_lss=[]
-    domestic_losses_lss=[]
-    exported_gains_lss=[]
-    exported_losses_lss=[]
+    #Resulting data frame where the results are in columnwise order
+    #in one excel sheet.
+    df_result_tC = pd.DataFrame()
     for country in countryls:
-        rowls1=[]
-        rowls2=[]
-        rowls3=[]
-        rowls4=[]
+        row_country_ls=[]
+        row_year_ls=[]
+        #Yhe lists for types of gains and losses 
+        row_tot_gains_type_ls=[]
+        row_tot_losses_type_ls=[]
+        row_domestic_gains_type_ls=[]
+        row_domestic_losses_type_ls=[]
+        row_exported_gains_type_ls=[]
+        row_exported_losses_type_ls=[]
+        #The lists for gains and losses
+        row_tot_gains_tC_ls=[]
+        row_tot_losses_tC_ls=[]
+        row_domestic_gains_tC_ls=[]
+        row_domestic_losses_tC_ls=[]
+        row_exported_gains_tC_ls=[]
+        row_exported_losses_tC_ls=[]
         #List all excel files and sort the files in ascending order (1990,1991,...,2021)
         #Exclude years in 1980's that some countries report
         excelfilels=list(set(glob.glob(directory+'/'+country+'/*.xlsx'))-set(glob.glob(directory+'/'+country+'/*_198??*.xlsx')))
         excelfilels=sorted(excelfilels)
         print(country,sheet)
+        year = start
+        #Some countries report HWP tot only
+        is_hwptot_only = False
         for excel_file in excelfilels:
-            xlsx = pd.ExcelFile(excel_file)
-            df = pd.read_excel(xlsx,sheet,keep_default_na=False,na_values=[''])
+            row_country_ls.append(country)
+            row_year_ls.append(year)
+            year = year+1
+            #xlsx_file = pd.ExcelFile(excel_file)
+            df = pd.read_excel(excel_file,sheet,keep_default_na=False,na_values=[''])
             #from_row  'Total' should give two rows exactly
-            rows2 = df[df[df.columns[0]].str.fullmatch(from_row)==True]
-            #Domestic gains
-            rowls1.append(rows2.iloc[0,col_gains])
-            #Domestic losses
-            rowls2.append(rows2.iloc[0,col_losses])
-            #Exported gains
-            rowls3.append(rows2.iloc[1,col_gains])
-            #Exported losses
-            rowls4.append(rows2.iloc[1,col_losses])
-            if np.isnan(rows2.iloc[0,col_gains]):
-                #Add country to total only 
-                hwp_total_only_set.add(country)
-                rows_tmp = df[df[df.columns[0]].str.contains(from_row_total)==True]
-                rowls1.pop()
-                rowls1.append(rows_tmp.iloc[1,col_gains])
-                rowls2.pop()
-                rowls2.append(rows_tmp.iloc[1,col_losses])
-        domestic_gains_lss.append(rowls1)
-        domestic_losses_lss.append(rowls2)
-        exported_gains_lss.append(rowls3)
-        exported_losses_lss.append(rowls4)
-    df_domestic_gains = pd.DataFrame(domestic_gains_lss)
-    df_domestic_losses = pd.DataFrame(domestic_losses_lss)
-    df_exported_gains = pd.DataFrame(exported_gains_lss)
-    df_exported_losses = pd.DataFrame(exported_losses_lss)
-    df_domestic_gains.index = countryls
-    df_domestic_gains.columns = list(range(start,end+1))
-    df_domestic_losses.index = countryls
-    df_domestic_losses.columns = list(range(start,end+1))
-    df_exported_gains.index = countryls
-    df_exported_gains.columns = list(range(start,end+1))
-    df_exported_losses.index = countryls
-    df_exported_losses.columns = list(range(start,end+1))
-    #Coloring HWP Total only country rows
-    countrlyls = sorted(list(hwp_total_only_set))
-    print("COUNTRIES",countrlyls)
-    idx = pd.IndexSlice
-    print(idx)
-    idx_slice = idx['AUT',:]
-    df_domestic_gains = df_domestic_gains.style.apply(color_orange,subset=idx_slice,axis=None)
-    df_domestic_gains.to_excel(excel_writer,hwp_sheet_name_ls[0],na_rep='NaN')
-    df_domestic_losses.to_excel(excel_writer,hwp_sheet_name_ls[1],na_rep='NaN')
-    df_exported_gains.to_excel(excel_writer,hwp_sheet_name_ls[2],na_rep='NaN')
-    df_exported_losses.to_excel(excel_writer,hwp_sheet_name_ls[3],na_rep='NaN')
+            rows1 = df[df[df.columns[0]].str.fullmatch(from_row)==True]
+            if np.isnan(rows1.iloc[0,col_gains]):
+                #Country has HWP total only
+                rows1 = df[df[df.columns[0]].str.contains(from_row_total)==True]
+                row_tot_gains_type_ls.append('TG')
+                row_tot_losses_type_ls.append('TL')
+                row_tot_gains_tC_ls.append(rows1.iloc[1,col_gains])
+                row_tot_losses_tC_ls.append(rows1.iloc[1,col_losses])
+                is_hwptot_only = True
+            else:
+                row_domestic_gains_type_ls.append('DG')
+                row_domestic_losses_type_ls.append('DL')
+                row_exported_gains_type_ls.append('EG')
+                row_exported_losses_type_ls.append('EL')
+                #Domestic gains
+                row_domestic_gains_tC_ls.append(rows1.iloc[0,col_gains])
+                #Domestic losses
+                row_domestic_losses_tC_ls.append(rows1.iloc[0,col_losses])
+                #Exported gains
+                row_exported_gains_tC_ls.append(rows1.iloc[1,col_gains])
+                #Exported losses
+                row_exported_losses_tC_ls.append(rows1.iloc[1,col_losses])
+        #One country is done. Collect the results in a list (list of lists), make a data frame *and transpose it*.
+        #Append to the final data frame. The results are now in columnwise order
+        country_lss1=[]
+        country_lss2=[]
+        if is_hwptot_only:
+            row_tot_gains_CO2_ls = [convert_co2(x) for x in row_tot_gains_tC_ls]
+            row_tot_losses_CO2_ls = [convert_co2(x) for x in row_tot_losses_tC_ls]
+            country_lss1 = [row_country_ls,row_year_ls,row_tot_gains_tC_ls,row_tot_gains_CO2_ls ,row_tot_gains_type_ls,
+                            row_tot_losses_tC_ls,row_tot_losses_CO2_ls,row_tot_losses_type_ls]
+        else:
+            row_domestic_gains_CO2_ls = [convert_co2(x) for x in row_domestic_gains_tC_ls]
+            row_domestic_losses_CO2_ls = [convert_co2(x) for x in row_domestic_losses_tC_ls]
+            row_exported_gains_CO2_ls = [convert_co2(x) for x in row_exported_gains_tC_ls]
+            row_exported_losses_CO2_ls = [convert_co2(x) for x in row_exported_losses_tC_ls]
+            country_lss1 = [row_country_ls,row_year_ls,
+                            row_domestic_gains_tC_ls,row_domestic_gains_CO2_ls,row_domestic_gains_type_ls,
+                            row_domestic_losses_tC_ls,row_domestic_losses_CO2_ls,row_domestic_losses_type_ls]
+            country_lss2 = [row_country_ls,row_year_ls,
+                            row_exported_gains_tC_ls,row_exported_gains_CO2_ls,row_exported_gains_type_ls,
+                            row_exported_losses_tC_ls,row_exported_losses_CO2_ls,row_exported_losses_type_ls]
+        
+        #Create dataframe for one country
+        df1_country = pd.DataFrame(country_lss1)
+        df2_country = pd.DataFrame(country_lss2)
+        #Transpose to get columnwise results
+        df1T_country = df1_country.transpose()
+        df2T_country = df2_country.transpose()
+        #Append to final dataframe
+        df_result_tC = pd.concat([df_result_tC,df1T_country,df2T_country])
+    #All countries collected, insert dataframe to excel
+    df_result_tC.columns = hwp_columns_ls 
+    df_result_tC.to_excel(excel_writer,hwp_sheet_name,na_rep='NaN',engine='xlsxwriter')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
